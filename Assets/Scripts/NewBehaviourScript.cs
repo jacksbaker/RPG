4596fc5356c2fcae6d9464b877f35d3b6b162c2e
Jack Baker <163625@escg.ac.uk>
2020-11-03 09:23:18 +0000

    no message

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hellow Jamie");
-    }
+//public class NewBehaviourScript : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    bool samPog = false;
 
-    // Update is called once per frame
-    void Update()
-    {
+//    void Start()
+//    {
+//        Debug.Log("Hellow Jamie");
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
