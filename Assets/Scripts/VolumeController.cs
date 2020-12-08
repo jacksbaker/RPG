15c333a3889a188c37dce75f49c3d98994ea03b3
JackBaker1 <163625@chrome.escg.ac.uk>
2020-12-08 19:46:59 +0000

    Added volume levels

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class VolumeController : MonoBehaviour
 {
+
+    private AudioSource theAudio;
+
+    private float audioLevel;
+    public float defaultAudio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetAudioLevel(float volume)
+    {
+        if(theAudio == null)
+        {
+            theAudio = GetComponent<AudioSource>();
+        }
+        audioLevel = defaultAudio * volume;
+        theAudio.volume = audioLevel; 
     }
 }
