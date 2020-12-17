c3fcee754c7c70747ff96f680a46e2b320cc7bf1
JackBaker1 <163625@chrome.escg.ac.uk>
2020-12-17 11:06:09 +0000

    Finished gane

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+
+    private static bool audioExists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!audioExists)
+        {
+            audioExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
